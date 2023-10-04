9cb73635a081254aa7d2cf4672a26502243b9a1b
kingxl111 <kingxl111@mail.ru>
2023-10-04 13:07:21 +0300

    multithreading added

--- lab2/src/main.cpp
+++ lab2/src/main.cpp
@@ -1,8 +1,6 @@
 #include <iostream>
 #include <pthread.h>
-#include <unistd.h>
 #include <time.h>
-#include <vector>
 
 using namespace std;
 
@@ -20,52 +18,35 @@ struct pthread_data {
     int j_finish;
 };
 
-
-pthread_mutex_t mutex;
-
 void* convolution(void* data){
 
     pthread_data* pdata = (pthread_data*)data;
-
-    int n = pdata->n;
-    int m = pdata->m;
-    double** mtx = pdata->user_matrix;
-    double** filter = pdata->filter;
-    double** new_matrix = pdata->result;
-    int i_start = pdata->i_start;
-    int j_start = pdata->j_start;
-    int i_finish = pdata->i_finish;
-    int j_finish = pdata->j_finish;
-
     
-    if(n == 0) {
+    if(pdata->n == 0) {
         return nullptr;
     }
+    // cout << "i_start: " << pdata->i_start << ", i_finish: " << pdata->i_finish << ", j_start: " << pdata->j_start << ", j_finish: " << pdata->j_finish << endl;
 
-    for(int i = i_start; i <= i_finish; ++i) {    
-        for(int j = j_start; j <= j_finish; ++j) {    
+    for(int i = pdata->i_start; i <= pdata->i_finish; ++i) {    
+        for(int j = pdata->j_start; j <= pdata->j_finish; ++j) {    
             double result = 0;
             int filter_x = 0;
             for(int x = i - FILTER_MATRIX_SIZE / 2; x <= i + FILTER_MATRIX_SIZE / 2; ++x) {
                 int filter_y = 0;
                 for (int y = j - FILTER_MATRIX_SIZE / 2; y <= j + FILTER_MATRIX_SIZE / 2; ++y) {
-                    if((x >= 0) && (x < n) && (y >= 0) && (y < m) 
+                    if((x >= 0) && (x < pdata->n) && (y >= 0) && (y < pdata->m) 
                     && ((filter_x < FILTER_MATRIX_SIZE) && (filter_y < FILTER_MATRIX_SIZE))) {
-                        // pthread_mutex_lock(&mutex);
-                        result += (mtx[x][y] * filter[filter_x][filter_y]);
-                        // pthread_mutex_unlock(&mutex);
+                        result += (pdata->user_matrix[x][y] * pdata->filter[filter_x][filter_y]);
                     }
                     ++filter_y;
                 }
                 ++filter_x;
             }
-            // pthread_mutex_lock(&mutex);
-            new_matrix[i][j] = result;
-            // pthread_mutex_unlock(&mutex);
-            
+            pdata->result[i][j] = result;
+            // cout << "result: " <<  pdata->result[i][j] << ", i = " << i << ", j = " << j << endl;
         }
     }
-    pthread_exit(0);
+    // pthread_exit(0);
     return nullptr;
 }
 
@@ -87,7 +68,7 @@ int main(int argc, char* argv[]) {
     cout << "m: ";
     cin >> m;
     cout << "Input the matrix" << endl;
-
+    
     double** user_matrix = (double**) malloc(n * sizeof(double*));
     for(int i = 0; i < n; ++i) {
         user_matrix[i] = (double*) malloc(m * sizeof(double));
@@ -106,39 +87,39 @@ int main(int argc, char* argv[]) {
     }
 
     double** filter1 = (double**) malloc(FILTER_MATRIX_SIZE * sizeof(double*));
-    for(int i = 0; i < n; ++i) {
-        filter1[i] = (double*) malloc(m * sizeof(double));
+    for(int i = 0; i < FILTER_MATRIX_SIZE; ++i) {
+        filter1[i] = (double*) malloc(FILTER_MATRIX_SIZE * sizeof(double));
     }
+
     for(int i = 0; i < FILTER_MATRIX_SIZE; ++i) {
         for(int j = 0; j < FILTER_MATRIX_SIZE; ++j) {
             filter1[i][j] = 1;
         }
     }
 
-    pthread_data pdata;
-    pdata.n = n;
-    pdata.m = m;
-    pdata.filter = filter1;
-    pdata.result = result;
-    pdata.user_matrix = user_matrix;
-
-
-    
     const int thread_count = stoi(argv[1]);
-    // cout << "Thread count: " << thread_count << endl;
+
+    pthread_data pdata[thread_count];
+    for(int i = 0; i < thread_count; ++i) {
+        pdata[i].n = n;
+        pdata[i].m = m;
+        pdata[i].filter = filter1;
+        pdata[i].result = result;
+        pdata[i].user_matrix = user_matrix;
+    }
+    
     pthread_t tid[thread_count];
-    pthread_mutex_init(&mutex, nullptr);
 
     int i_start = 0;
-    int i_finish = i_start + n / thread_count - 1;
+    int i_finish = (i_start + n / thread_count - 1) > 0 ? i_start + n / thread_count - 1 : 0;
     int j_start = 0;
     int j_finish = m - 1;
     for(int i = 0; i < thread_count; ++i) {
-        pdata.i_start = i_start;
-        pdata.i_finish = i_finish;
-        pdata.j_start = j_start;
-        pdata.j_finish = j_finish;
-        cout << "i_start: " << i_start << ", i_finish: " << i_finish << ", j_start: " << j_start << ", j_finish: " << j_finish << endl;
+        pdata[i].i_start = i_start;
+        pdata[i].i_finish = i_finish;
+        pdata[i].j_start = j_start;
+        pdata[i].j_finish = j_finish;
+        // cout << "i_start: " << i_start << ", i_finish: " << i_finish << ", j_start: " << j_start << ", j_finish: " << j_finish << endl;
         i_start = (i_finish + 1);
         i_finish = (i_start + n / thread_count);
         if(i_start >= n) {
@@ -147,25 +128,16 @@ int main(int argc, char* argv[]) {
         if(i_finish >= n) {
             i_finish = n - 1;
         }
-        if(pthread_create(&tid[i], nullptr, convolution, &pdata) != 0) {
+        
+        if(pthread_create(&tid[i], NULL, convolution, &pdata[i]) != 0) {
             return 1;
         }
     }
     for(int i = 0; i < thread_count; ++i) {
-        if(pthread_join(tid[i], nullptr) != 0) {
+        if(pthread_join(tid[i], NULL) != 0) {
             return 1;
         }
     }
-    
-
-    //The first arg is a number of threads
-
-    // cout << "OK" << endl;
-    // for (int i = 0; i < n; ++i) {
-    //     for(int j = 0; j < m; ++j) {
-    //         convolution(users_matrix, i, j, filter1, result); 
-    //     } 
-    // }
 
     for (int i = 0; i < n; ++i) {
         for(int j = 0; j < m; ++j) {
@@ -173,12 +145,10 @@ int main(int argc, char* argv[]) {
         } cout << endl;
     }
 
-    pthread_mutex_destroy(&mutex);
-
     free(user_matrix);
     free(filter1);
     free(result);
 
-    cout << (double)(clock() - start ) / CLOCKS_PER_SEC << " seconds" << endl;
+    cout << "Total time: "<< (double)(clock() - start ) / CLOCKS_PER_SEC << " seconds" << endl;
     return 0;
 }
